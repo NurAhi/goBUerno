d97aee9db4f389ed41684a2768fc7575d78d84c5
NurAhi <nurahi10@gmail.com>
2023-01-20 15:51:38 +0100

    feat: add image

--- src/App.tsx
+++ src/App.tsx
@@ -40,51 +40,47 @@ function App() {
           <div>
             {person.name === "Cristina Ruiz Trescastro" ? (
               <li key={index}>
-                <div
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                    borderRadius: "50%",
-                  }}
-                />
-                <p style={{ fontWeight: "bold", fontSize: "20px" }}>
-                  ¡¡¡ENHORABUENA {person.name}!!!
-                </p>
-                <p style={{ lineHeight: "24px" }}>
-                  Es increíble que ya estemos en ese día tan esperado, temido y
-                  soñado...
-                </p>
-                <p>
-                  Parece que fue ayer cuando tu hermana Mokito comprobaba que
-                  sacabas esos pedazo de 10!!! Y el orgullo que sentimos todos
-                  es enorme,{" "}
-                  <strong style={{ fontWeight: "bold" }}>
-                    ¡LO HAS LOGRADO!
-                  </strong>{" "}
-                  y a pesar de todo.
-                </p>
-                <p>
-                  Desde luego se puede decir que todo en la vida nos aporta algo
-                  y tú, ante esa adversidad allá por el 2015 supiste crecer como
-                  nadie, es para sentirse orgullosa.
-                </p>
-                <p>
-                  Lo que hoy pase ahí dentro es solo un pasito más. Obviamente
-                  importante y nos imaginamos lo nerviosa que puedes estar pero
-                  confía en ti, en la buena cabeza que siempre has tenido y esto
-                  no es una excepción.
-                </p>
-                <p>
-                  Te queremos mucho muchas personas a las que cuidas cada día y
-                  de las que estás pendiente, pese a la distancia, a tu poca
-                  disponibilidad horaria para hablar, pero siempre encuentras
-                  hueco.
-                </p>
-                <p>
-                  Hoy será el comienzo de algo distinto, otra etapa y seguro que
-                  la disfrutas como haces con todo.
-                </p>
-                <p>¡¡¡SUERTE!!!</p>
+                <div className="c-image" />
+                <div>
+                  <p style={{ fontWeight: "bold", fontSize: "20px" }}>
+                    ¡¡¡ENHORABUENA {person.name}!!!
+                  </p>
+                  <p style={{ lineHeight: "24px" }}>
+                    Es increíble que ya estemos en ese día tan esperado, temido
+                    y soñado...
+                  </p>
+                  <p>
+                    Parece que fue ayer cuando tu hermana Mokito comprobaba que
+                    sacabas esos pedazo de 10!!! Y el orgullo que sentimos todos
+                    es enorme,{" "}
+                    <strong style={{ fontWeight: "bold" }}>
+                      ¡LO HAS LOGRADO!
+                    </strong>{" "}
+                    y a pesar de todo.
+                  </p>
+                  <p>
+                    Desde luego se puede decir que todo en la vida nos aporta
+                    algo y tú, ante esa adversidad allá por el 2015 supiste
+                    crecer como nadie, es para sentirse orgullosa.
+                  </p>
+                  <p>
+                    Lo que hoy pase ahí dentro es solo un pasito más. Obviamente
+                    importante y nos imaginamos lo nerviosa que puedes estar
+                    pero confía en ti, en la buena cabeza que siempre has tenido
+                    y esto no es una excepción.
+                  </p>
+                  <p>
+                    Te queremos mucho muchas personas a las que cuidas cada día
+                    y de las que estás pendiente, pese a la distancia, a tu poca
+                    disponibilidad horaria para hablar, pero siempre encuentras
+                    hueco.
+                  </p>
+                  <p>
+                    Hoy será el comienzo de algo distinto, otra etapa y seguro
+                    que la disfrutas como haces con todo.
+                  </p>
+                  <p>¡¡¡SUERTE!!!</p>
+                </div>
               </li>
             ) : (
               <li>
